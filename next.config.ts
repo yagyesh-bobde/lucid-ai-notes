6df2e7c99e375e076f10c0a97439e9cab4c320c3
yagyesh-bobde <yagyeshsrbobde@gmail.com>
2025-04-23 20:36:29 +0530

    feat: remove unused components

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,12 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Ensure that all imports of 'yjs' resolve to the same instance
-      config.resolve.alias['yjs'] = path.resolve(__dirname, 'node_modules/yjs')
-    }
-    return config
-  },
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // }
 };
 
 export default nextConfig;
